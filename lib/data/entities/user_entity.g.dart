337a97093ba0061a0c7edb4d875ef9efeaea3883
purba.arkademi@gmail.com <purba.arkademi@gmail.com>
2024-01-26 05:43:25 +0700

    change type data updated at and created at

--- lib/data/entities/user_entity.g.dart
+++ lib/data/entities/user_entity.g.dart
@@ -10,11 +10,11 @@ UserEntity _$UserEntityFromJson(Map<String, dynamic> json) => UserEntity(
       id: json['id'] as String?,
       email: json['email'] as String?,
       password: json['password'] as String?,
-      createdAt: json['created_at'] as int?,
+      createdAt: json['created_at'] as String?,
       phoneNumber: json['phone'] as String?,
       userName: json['username'] as String?,
       avatarUrl: json['avatar_url'] as String?,
-      updatedAt: json['updated_at'] as int?,
+      updatedAt: json['updated_at'] as String?,
     );
 
 Map<String, dynamic> _$UserEntityToJson(UserEntity instance) =>
