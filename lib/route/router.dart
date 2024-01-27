113d37be4b0085b45a4a53ac19380944d05fe1c0
purba.arkademi@gmail.com <purba.arkademi@gmail.com>
2024-01-27 21:49:07 +0700

    change naming file chat room

--- lib/route/router.dart
+++ lib/route/router.dart
@@ -1,6 +1,6 @@
 import 'package:chat_app/route/route_name.dart';
 import 'package:chat_app/screens/authentication_screen.dart';
-import 'package:chat_app/screens/home_screen.dart';
+import 'package:chat_app/screens/messages_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
