8b6b6cd75a7738ffa08c0e07c7727c50fe6afd4f
purba.arkademi@gmail.com <purba.arkademi@gmail.com>
2024-01-28 22:04:05 +0700

    api client create message

--- lib/services/api_client.dart
+++ lib/services/api_client.dart
@@ -5,6 +5,7 @@ import 'dart:io';
 
 import 'package:chat_app/data/entities/user_entity.dart';
 import 'package:http/http.dart' as http;
+import 'package:models/models.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 typedef TokenProvider = Future<String?> Function();
@@ -30,13 +31,6 @@ class ApiClient {
   final String _baseUrl;
   final http.Client _httpClient;
 
-  Future<Map<String, dynamic>> fetchMessages(String chatRoomId) async {
-    final uri = Uri.parse('$_baseUrl/messages/id/$chatRoomId/message');
-    final response = await _handleRequest(
-        (headers) => _httpClient.get(uri, headers: headers));
-    return response;
-  }
-
   Future<List<UserEntity>> searchUser(String query, String token) async {
     List<UserEntity> users = [];
     final uri = Uri.parse('$_baseUrl/search?name=$query');
@@ -55,16 +49,37 @@ class ApiClient {
     return users;
   }
 
+  Future<void> createMessage(Message message, String token) async {
+    final uri = Uri.parse("$_baseUrl/messages/create-message");
+    final response =
+        await http.post(uri, body: jsonEncode(message.toJson()), headers: {
+      HttpHeaders.authorizationHeader: token,
+    });
+
+    if (response.statusCode != HttpStatus.ok) {
+      throw Exception("${response.statusCode}, error: ${response.body}");
+    }
+  }
+
+  Future<Map<String, dynamic>> fetchMessages(String chatRoomId) async {
+    final uri = Uri.parse('$_baseUrl/messages/id/$chatRoomId/message');
+    final response = await _handleRequest(
+        (headers) => _httpClient.get(uri, headers: headers));
+    return response;
+  }
+
   Future<List<Map<String, dynamic>>> fetchAllMessages() async {
     final prefs = await SharedPreferences.getInstance();
     final token = prefs.getString("token") ?? "";
     final uri = Uri.parse('$_baseUrl/messages');
-    final response = await http.get(uri, headers: {"Authorization": token});
+    final response =
+        await http.get(uri, headers: {HttpHeaders.authorizationHeader: token});
     final decode = jsonDecode(response.body);
-    if (decode != null) {
+    if (decode == null) {
       return [];
     }
     final data = List<Map<String, dynamic>>.from(decode["data"]);
+
     return data;
   }
 
