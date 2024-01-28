aae9026b1d841b42a27a6252b527d6c71f839122
purba.arkademi@gmail.com <purba.arkademi@gmail.com>
2024-01-28 08:29:47 +0700

    inject search repo

--- lib/di/dependency_injection.dart
+++ lib/di/dependency_injection.dart
@@ -1,7 +1,9 @@
 import 'package:chat_app/data/repositories/auth_repository_impl.dart';
 import 'package:chat_app/data/repositories/message_repository_impl.dart';
+import 'package:chat_app/data/repositories/search_repository_impl.dart';
 import 'package:chat_app/domain/repositories/auth_repository.dart';
 import 'package:chat_app/domain/repositories/message_repository.dart';
+import 'package:chat_app/domain/repositories/search_repository.dart';
 import 'package:chat_app/services/api_client.dart';
 import 'package:get_it/get_it.dart';
 
@@ -21,5 +23,8 @@ class DependencyInjection {
 
     getIt.registerLazySingleton<AuthRepository>(
         () => AuthRepositoryImpl(getIt.get()));
+
+    getIt.registerLazySingleton<SearchRepository>(
+        () => SearchRepositoryImpl(getIt.get()));
   }
 }
