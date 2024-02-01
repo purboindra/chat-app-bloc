87b1ee724284cc1c729a821f26eccd012fa27585
purba.arkademi@gmail.com <purba.arkademi@gmail.com>
2024-02-02 05:17:45 +0700

    subscribe message function

--- lib/domain/repositories/message_repository.dart
+++ lib/domain/repositories/message_repository.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:chat_app/data/entities/chat_room_entity.dart';
 import 'package:chat_app/data/entities/message_entity.dart';
 import 'package:models/models.dart';
@@ -6,4 +8,7 @@ abstract class MessageRepository {
   Future<List<ChatRoomEntity>> fetchAllMessages();
   Future<List<MessageEntity>> fetchMessages(String chatRoomId);
   Future<void> createMessage(Message message, String token);
+  void subscribeToMessageUpdate(
+      void Function(Map<String, dynamic>) onMEssageReceived);
+  void unSubscribeFromMessageUpdates(StreamSubscription streamSubscription);
 }
